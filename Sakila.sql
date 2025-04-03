9c1af96c63f922cd3706c70d41ba906900e60efe
Mary Llusco <maryluzllusco03@gmail.com>
2025-04-03 11:50:33 -0300

    hicimos mas ejercicios

--- Sakila.sql
+++ Sakila.sql
@@ -13,3 +13,29 @@ INNER JOIN film_category fc ON  f.film_id = fc.film_id
 INNER JOIN category c ON  c.category_id = fc.category_id
 INNER JOIN language l ON l.language_id = f.language_id
 WHERE length BETWEEN 60 AND 120
+
+/*Ejercicio 3*/
+
+SELECT s.first_name, s.last_name, a.address, c.city, co.country FROM staff s
+INNER JOIN address a ON s.address_id= a.address_id
+INNER JOIN city c ON a.city_id= c.city_id
+INNER JOIN country co ON c.country_id = co.country_id
+
+/*ejercicio 4*/
+
+SELECT max (return_date), min (return_date), f.title FROM film f
+INNER JOIN inventory i ON f.film_id= i.film_id
+INNER JOIN rental r ON i.inventory_id= r.inventory_id
+
+/*ejercicio 5*/
+
+
+/*ejercicio 6*/
+SELECT count (title), f.rating FROM film f
+GROUP BY rating
+
+/*ejercicio 7*/
+SELECT count (name), c.category_id FROM category c
+GROUP BY category_id
+
+
