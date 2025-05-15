faa07b6c225270671b8df7293b89faf6e06cd82a
mary llusco <maryluzllusco03@gmail.com>
2025-05-15 11:56:23 -0300

    termine los ejercicios de sakila y chinook, de hijo lo tengo al chinook

--- Sakila.sql
+++ Sakila.sql
@@ -39,5 +39,103 @@ SELECT count (name), c.category_id FROM category c
 GROUP BY category_id
 
 /*ejercicio 8*/
+SELECT a.first_name,a.last_name,count(a.actor_id) as cant_actor, f.title
+FROM film f
+INNER JOIN film_actor fa ON f.film_id = fa.film_id
+INNER JOIN actor a ON fa.actor_id = a.actor_id
+GROUP BY f.film_id
+ORDER by cant_actor desc limit 10
 
+/*ejercicio 9*/
+SELECT a.address, c.city, co.country,count(i.inventory_id)
+FROM inventory i
+INNER JOIN store s ON i.store_id = s.store_id
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON a.city_id = c.city_id
+INNER JOIN country co ON c.country_id = co.country_id  
+GROUP by a.address
+
+/*ejercicio 10*/
+SELECT a.address, c.city, co.country,count(f.film_id)
+FROM film f
+INNER JOIN inventory i ON f.film_id = i.film_id
+INNER JOIN store s ON i.store_id = s.store_id
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON a.city_id = c.city_id
+INNER JOIN country co ON c.country_id = co.country_id  
+GROUP by a.address
+
+/*ejercicio 11*/
+SELECT AVG(f.rental_duration * f.rental_rate ) AS costo_promedio, c.name
+FROM film f
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+GROUP by c.name
+
+/*ejercicio 12*/
+SELECT f.title AS "Película", r.rental_date AS "Día que se alquiló", r.return_date AS "Día que se devolvió",
+r.return_date - r.rental_date AS "Días de Alquiler", p.amount AS "Costo Total", p.amount / (r.return_date - r.rental_date)/* la barra (/), divide*/
+AS "Costo por Día" FROM rental r
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+JOIN payment p ON r.rental_id = p.rental_id
+WHERE f.title = 'ALABAMA DEVIL'
+ORDER BY r.rental_date DESC;
+
+/*ejercicio 13*/
+SELECT f.title, f.length, c.name
+FROM film f
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+ORDER BY f.length DESC;
+
+/*ejercicio 14*/
+SELECT  film.title
+FROM film
+INNER JOIN film_actor ON film.film_id = film_actor.film_id
+WHERE film.title LIKE 'W%'/*solo los que empiecen con esa letra*/
+GROUP BY film.film_id, film.title
+HAVING COUNT(film_actor.actor_id) >= 5; /*garantiza que solo se muestren películas con 5 o más actores*/
+
+/*ejercicio 15*/
+SELECT c.last_name As "Apellido", c.first_name AS "Nombre", SUM(p.amount) AS "Pagos" FROM customer c
+INNER JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY c.customer_id, c.first_name, c.last_name
+ORDER BY "Total de Pagos" DESC;
+
+/*ejercicio 16*/
+SELECT  f.title, f.length, a.first_name, a.last_name
+FROM film f
+INNER JOIN film_actor fa ON f.film_id = fa.film_id
+INNER JOIN actor a ON fa.actor_id = a.actor_id
+ORDER BY f.length ASC
+LIMIT 1; /*agarra la pelicula mas corta, de toda la columna */
+
+/*ejercicio 17*/
+SELECT c.last_name AS "Apellido", a.address AS "Dirección",  ci.city AS "Ciudad", co.country AS "País", r.rental_date AS "Fecha de alquiler",
+p.payment_date AS "Fecha de pago", p.amount AS "Monto del pago" FROM customer c
+INNER JOIN address a ON c.address_id = a.address_id
+INNER JOIN city ci ON a.city_id = ci.city_id
+INNER JOIN country co ON ci.country_id = co.country_id
+INNER JOIN rental r ON c.customer_id = r.customer_id
+INNER JOIN payment p ON r.rental_id = p.rental_id
+ORDER BY p.amount ASC;
+
+/*ejercicio 18*/
+INSERT INTO actor (actor_id, first_name, last_name, last_update)/*año,mes,dia,hora*/
+VALUES (203, 'mary', 'llusco', '2025-05-15 09:47:29');
+
+/*ejercicio 19*/
+INSERT INTO actor (actor_id,first_name, last_name,last_update)
+VALUES (201,'Carlos', 'Ramírez', '2025-05-15 09:46:29'), (202,'Lucía', 'Acuña', '2025-05-15 09:47:29');
+
+/*ejercicio 20*/
+SELECT actor_id, first_name, last_name FROM actor
+WHERE first_name IN ('Carlos', 'Lucía', 'mary');
+UPDATE actor SET first_name = 'Leonardo', last_name = 'DiCaprio' WHERE actor_id = 201;
+UPDATE actor SET first_name = 'Scarlett', last_name = 'Johansson' WHERE actor_id = 202;
+UPDATE actor SET first_name = 'Morgan', last_name = 'Freeman' WHERE actor_id = 203; 
+
+/*ejercicio 21*/
+DELETE FROM actor WHERE actor_id IN (201, 202, 203);
 
--- chino.sql
+++ chino.sql
@@ -1,9 +1,85 @@
 /*ejercicio 1*/
 select FirstName, LastName from employees 
 order by FirstName ASC, LastName ASC
+
 /*ejercicio 2*/
 select t.name, t.Milliseconds from tracks t 
 INNER JOIN albums a ON t.AlbumId = t.AlbumId
 where Title like "Big Ones" 
 order by Milliseconds ASC
+
 /*ejercicio 3*/
+SELECT g.Name, COUNT(t.TrackId) AS cantidad_canciones
+FROM  genres g
+INNER JOIN tracks t ON g.GenreId = t.GenreId
+GROUP BY g.GenreId, g.Name
+ORDER BY  cantidad_canciones DESC;
+
+/*ejercicio 4*/
+SELECT a.Title, count(t.TrackId)/*muestra en la pantalla*/
+FROM albums a
+INNER JOIN tracks t ON a.AlbumId = t.AlbumId
+GROUP BY a.AlbumId, a.Title
+HAVING COUNT(t.TrackId) >= 5;
+
+/*ejercicio 5*/
+SELECT a.Title AS albums, SUM(t.UnitPrice) AS Preciototal
+FROM albums a
+INNER JOIN tracks t ON a.AlbumId = t.AlbumId
+GROUP BY a.AlbumId
+ORDER BY Preciototal ASC
+LIMIT 10;
+
+/*ejercicio 6*/
+SELECT g.Name AS "Género", al.Title AS "Nombre del disco", t.name AS "Nombre de la canción" FROM tracks t
+INNER JOIN genres g ON t.GenreId = g.GenreId
+INNER JOIN albums al ON t.AlbumId = al.AlbumId
+WHERE t.UnitPrice = 0.99;
+
+/*ejercicio 7*/
+SELECT  t.name, t.Milliseconds, a.Title, at.name
+FROM  tracks t
+INNER JOIN albums a ON t.AlbumId = a.AlbumId
+INNER JOIN artists at ON a.ArtistId = at.ArtistId
+ORDER BY t.Milliseconds ASC
+LIMIT 20;
+
+/*ejercicio 8*/
+SELECT e.LastName AS "Empleado", e.Title AS "Puesto", m.LastName AS "Apellido del jefe",
+COUNT(c.CustomerId) AS "Cantidad de clientes" FROM employees e
+INNER JOIN employees m ON e.ReportsTo = m.EmployeeId
+INNER JOIN customers c ON e.EmployeeId = c.SupportRepId
+GROUP BY e.EmployeeId, e.LastName, e.Title, m.LastName
+ORDER BY "Cantidad de Clientes" DESC;
+
+/*ejercicio 9*/
+INSERT INTO tracks (TrackId,Name, MediaTypeId, Milliseconds, UnitPrice)
+VALUES 
+(3512,'ramen para dos' ,1, 354000, 0.99),
+(3513,'ella' ,1, 354001, 0.99),
+(3514,'ciudad magica' ,1, 354002, 0.99),
+(3515,'acaramelao' ,1, 354003, 0.99);
+
+/*ejercicio 10*/
+SELECT t.TrackId, t.Name
+FROM tracks t
+ORDER BY  t.TrackId DESC
+LIMIT 4;
+
+/*ejercicio 11*/
+UPDATE tracks
+SET Name = 'Atardecer Digital'
+WHERE TrackId = 3512;
+
+UPDATE tracks
+SET Name = 'bronceado'
+WHERE TrackId = 3513;
+
+/*ejercicio 12*/
+SELECT * FROM tracks
+WHERE TrackId IN (3512, 3513);
+
+/*ejercicio 13*/
+DELETE FROM tracks
+WHERE TrackId IN (3512, 3513);
+
